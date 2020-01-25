b38073b707f25b4f781133030fa091b2c72cb848
jlyhtech <jlyhtech@gmail.com>
2020-01-25 12:36:54 -0800

    Initial Commit

---
+++ hello_metal/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  hello_metal
+//
+//  Created by Yihung Lee on 1/25/20.
+//  Copyright © 2020 Yihung Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
